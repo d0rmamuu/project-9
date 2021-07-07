b642d15cea9b89f972faedbb8bfbb9a02def27be
d0rmamuu <48427526+d0rmamuu@users.noreply.github.com>
2021-07-07 21:29:56 +0530

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export default function Footer() {
-  return (
-	<footer className="footer">
-        <p>©️powered by html 5™️,javascript™️,php™️&css™️ </p>
-        </footer> 
-  );}
